7cf210197e50ce1ce7f3b32a0851696900f41dad
D Paul Childs <clausewitzian@gmail.com>
2020-02-06 10:02:48 -0500

    init

---
+++ src/main/java/labelBuilder/EliphindSearch.java
@@ -0,0 +1,133 @@
+package labelBuilder;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EliphindSearch {
+    private ArrayList<String> headlines = new ArrayList<>();
+    private static String[] urls;
+
+
+
+    // construct search query, number of pages, list of urls as a result of those variables, and execute
+    public EliphindSearch(int pages, String query) {
+        urls = new String[pages];
+        urls = buildUrls(pages, query);
+    }
+
+    private String[] buildUrls(int pages, String query) {
+        if(pages == 1)
+            return new String[]{"https://elephind.com/?a=q&results=1&r=100&e=-------en-100--1--txt-txINtxCO-" + query + "---------"};
+        else {
+            String[] temp = new String[pages];
+            temp[0] = "https://elephind.com/?a=q&results=1&r=100&e=-------en-100--1--txt-txINtxCO-" + query + "---------";
+
+            for(int i = 1; i< pages; i++) {
+                temp[i] = "https://elephind.com/?a=q&results=1&r=" + i + "01&e=-------en-100--1--txt-txINtxCO-" + query + "---------";
+            }
+            return temp;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        // Multiple words must have '+' between them in the query
+        EliphindSearch eliphindSearch = new EliphindSearch(2, "iran+us+conflict");
+
+        List<PageRequest> pageRequestsList = new ArrayList<>(urls.length);
+        for(String url : urls) {
+            PageRequest page = new PageRequest(url);
+            pageRequestsList.add(page);
+            new Thread(page).start();
+        }
+
+        for(PageRequest request : pageRequestsList) {
+            Elements results = request.waitForResults();
+            if(results != null) {
+                for(Element each : results) {
+                    if(each.text().length() > 10)
+                        eliphindSearch.headlines.add(each.text());
+                }
+                    System.out.println("found " + eliphindSearch.headlines.size() + " records");
+            } else {
+                System.out.println("Error occurred");
+            }
+        }
+
+//        System.out.println(eliphindSearch.getHeadlines());
+        eliphindSearch.printHeadlinesToFile();
+
+    }
+
+
+
+    static class PageRequest implements Runnable {
+        private String url;
+        private Elements results;
+        private final Object lock = new Object();
+
+        public PageRequest(String url) throws IOException {
+            this.url = url;
+        }
+
+        @Override
+        public void run() {
+            try {
+                synchronized (lock) {
+                    Document doc = Jsoup.connect(url)
+                            .get();
+
+                    // Page specific parsing
+                    Elements content = doc.getElementsByClass("veridiansearchresultcell");
+
+                    this.results = content.select("div").select("a").select("span");
+                    lock.notifyAll();
+                }
+
+            } catch(IOException e) {
+                System.out.println(e);
+            }
+        }
+
+        public Elements waitForResults() {
+            synchronized (lock) {
+                try {
+                    while(this.results == null) {
+                        lock.wait();
+                    }
+                    return this.results;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }
+
+    }
+
+    public ArrayList<String> getHeadlines() {
+        return headlines;
+    }
+
+    public void printHeadlinesToFile() throws IOException {
+        File f = new File("test2.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(f.getAbsolutePath()));
+
+        for(int i = 0; i < headlines.size(); i++) {
+            bufferedWriter.write(headlines.get(i));
+            bufferedWriter.newLine();
+        }
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+
+}
---
+++ src/main/java/labelBuilder/labelBuilder.java
@@ -0,0 +1,49 @@
+package labelBuilder;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class labelBuilder {
+    private ArrayList<String> headlines;
+
+
+
+//    https://elephind.com/?a=q&r=1&results=1&o=100&e=-------en-10--1--txt-txINtxCO-iran+us+conflict---------
+
+
+    public static void main(String[] args) throws IOException {
+        String iran_us_conflict_search_Url = "https://elephind.com/?a=q&r=1&results=1&o=100&e=-------en-10--1--txt-txINtxCO-iran+us+conflict---------";
+
+//        labelBuilder labelBuilder = new labelBuilder();
+
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(""));
+
+//        EliphindSearch eliphindSearch = new EliphindSearch(2);
+
+
+
+
+//        ArrayList<String> temp = eliphindSearch.getHeadlines();
+
+//        System.out.println(temp);
+//        System.out.println(temp.size());
+
+
+//        for(int i = 0; i<temp.size(); i++) {
+//            bufferedWriter.write(temp.get(i));
+//            bufferedWriter.newLine();
+//            System.out.println(temp.get(i));
+//        }
+//        bufferedWriter.flush();
+//        bufferedWriter.close();
+
+    } // ends main
+
+}
